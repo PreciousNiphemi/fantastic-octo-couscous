653129a829fa711adeb1720fc3b6c503387d1f13
preciousniphemi <preciousoluwanifemi1999@gmail.com>
2023-04-09 15:50:46 +0100

    add functionality

--- index.js
+++ index.js
@@ -1,48 +1,82 @@
 
 
 // create an empty array to store objects
-// const people = [];
-
-// // get the form element and listen for the submit event
-// const form = document.getElementById("myForm");
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault(); // prevent form submission
-
-//   // get the values of the input fields
-//   const name = document.getElementById("name").value;
-//   const age = document.getElementById("age").value;
-//   const email = document.getElementById("email").value;
-
-//   // create a new object with the input values
-//   const person = { name, age, email };
-
-//   // add the new object to the array
-//   people.push(person);
-
-//   // clear the input fields
-//   form.reset();
-
-//   // update the list of objects displayed in the HTML
-//   const list = document.getElementById("list");
-//   list.innerHTML = ""; // clear the list
-//   people.forEach((person) => {
-//     const listItem = document.createElement("li");
-//     listItem.textContent = `${person.name}, ${person.age}, ${person.email}`;
-//     list.appendChild(listItem);
-//   });
-// });
+const people = [{
+    name:"John oils",
+    personelId:155
+}];
 
 
+function generateName() {
+    const possibleChars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=';
+    const nameLength = 10;
+    let newName = '';
+    
+    for (let i = 0; i < nameLength; i++) {
+      const randomIndex = Math.floor(Math.random() * possibleChars.length);
+      newName += possibleChars[randomIndex];
+    }
+    
+    return newName;
+ }
 
-// <form id="myForm">
-//   <label for="name">Name:</label>
-//   <input type="text" id="name" name="name"><br>
+ function updateDisplay() {
+    // get a reference to the div to populate
+    const displayDiv = document.getElementById('my-personel');
+    
+    // create a new HTML string to populate the div
+    let htmlString = '';
+    for (let i = 0; i < people.length; i++) {
+      const person = people[i];
+      htmlString += `<div class="wanted-personal-security-div">
+      <div>
+          <div class="personal-security-information-div">
+              <div class="character-box">
+                  <img class="character-image" src="./assets/character.png" />
+                  </div>
+              <div class="information-text-div">
+              <div >
+                  <p class="personel-name">${person.name}</p>
+                  <div class="personel-information">
+                      <p class="dull-personel-information-text">SOME INFORMATION</p>
+                      <div class="warrant-dot"></div>
+                      <p class="dull-personel-information-text">SOME INFORMATION</p>
+                      <div class="warrant-dot"></div>
+                      <p class="dull-personel-information-text">ID: ${person.personelId}</p>
+                  </div>
+              </div>
+              <div class="warrant-action-div">
+                  <button class="rejected-action-btn">
+                      WANTED
+                  </button>
+                 
+              </div>
 
-//   <label for="age">Age:</label>
-//   <input type="number" id="age" name="age"><br>
+              </div>
+              
+          </div>
 
-//   <label for="email">Email:</label>
-//   <input type="email" id="email" name="email"><br>
+      </div>
+  </div>`;
+    }
+    
+    // set the div's innerHTML to the new HTML string
+    displayDiv.innerHTML = htmlString;
+  }
 
-//   <button type="submit">Submit</button>
-// </form>
+
+
+function addPerson() {
+    // create a new object with the given name and age
+    const name = generateName();
+    const personelId = 18;
+
+    const personel = { name, personelId};
+    
+    // add the object to the people array
+    people.push(personel);
+    
+    // update the div with the new person's information
+    updateDisplay();
+  }
+
